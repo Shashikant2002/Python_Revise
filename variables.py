da616e8f2e758e50752b882392802a5e8499ea95
Shashikant <shashikantprajapti17@gmail.com>
2025-09-10 08:58:23 +0530

    ...

---
+++ variables.py
@@ -0,0 +1,16 @@
+# # frutes = ["new", 4, 5, True, "Akash"]
+# # frutes = [40, 4, 5, 20, 10]
+# frutes = ["amar", "chentu", "babu", "Agra"]
+
+# # frutes.append("5")
+# frutes.insert(2, "5")
+# # frutes.sort()
+# print(frutes)
+
+
+
+# Tupple 
+
+tup = (4,)
+
+print(type(tup))
