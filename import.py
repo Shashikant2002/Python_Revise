fad0fd5b12470b2fbbe9f1971117938b94225331
Shashikant <shashikant.prajapti@htdigital.in>
2025-10-25 21:16:10 +0530

    ...

---
+++ import.py
@@ -0,0 +1,6 @@
+from input import add
+
+result = add()
+print(result)
+
+
--- input.py
+++ input.py
@@ -1,6 +1,11 @@
-a = int(input("Enter a"))
-b = int(input("Enter b"))
 
 
-print(a-b)
 
+def add():
+    a = int(input("Enter a"))
+    b = int(input("Enter b"))
+    return a + b
+
+
+
+print(__name__)
